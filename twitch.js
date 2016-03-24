e03d4377466a18a9d1d2cd8c55443d86e688cc5f
daansteraan <d.strijdom@gmail.com>
2016-03-24 12:32:07 +0200

    Last version from codepen...

--- twitch.js
+++ twitch.js
@@ -1,46 +1,89 @@
-/* USERS */
-var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","monstercat"];
+/*all users*/
+  var all = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","monstercat"];   
 
+/*API bases*/
 var streamAPI = "https://api.twitch.tv/kraken/streams/";
+
+/*this one to be used later to get details of stream as well as avatar*/
 var channelAPI = "https://api.twitch.tv/kraken/channels";
 
-/*populate grid function
-default on all
-sub-fn - remove offline & remove online;
-*/
+
+/*function to fill the grid on the page - GOOD*/
 function populateGrid(users) {
 	var html = '';
+	
+	/*for loop that takes each value in the list provided and puts it on the page in the form of an unordered list*/
 	for (i=0; i<users.length; i++) {
-		$(".user-list").html(html += "<ul>"+users[i] + "</ul>")
-		
+		$(".user-list").html(html += "<ul>"+users[i] + "</ul>")	
 	};
 };
 
-populateGrid(users)
-
-/*IS ONLINE function - 
-return true/false
-if true & 
-
-
-user json url - wrap this is a function(u,api)*/
+/*takes a user, returns boolean value of online/offline status*/
 function isOnline(user) {
-$.getJSON(streamAPI + user, function(data){
-	
-  var available = data.stream;            
-  /*redundancy?*/
-  if (available) {
-	return true;
-  } else {
-    return false;
-  };
-          
+	  $.getJSON(streamAPI + user + "?callback=?", function(data){	
+	    
+      var available = data.stream;        	    
+		  /*redundancy?*/
+		  if (available) {
+        online.push(user);
+      } else {
+        offline.push(user)
+      };
+	  });
+};  
+
+function isOnlineTest(group){
+  group.forEach(function(i){
+    isOnline(i);
   });
+}
+
+/*Create online & offline lists globally - perhaps later to move into display() function*/
+var online = [];
+var offline = [];
+
+all.forEach(function(user) {
+  isOnline(user);
+  
+})
+
+console.log("on: " + online);
+console.log("off: " + offline);
+
+
+/*main purpose of this function is to display the correct list of users*/
+function display(users) {
+	
+	console.log("CALLING DISPLAY FUNCTION");
+    
+	/*forloop iterating through users and sorting into online/offline lists*/
+  var online = [];
+  var offline = [];	
+  
+  users.forEach(function(user) { 
+    var count = isOnline(user);
+    online.push(count);
+    });
+
+  console.log("online: " + online);
+  
+  
+  var x = $('input', this).attr("checked");
+  /*should work up to this point, i*/
+	if ($('input[id=all]:checked').length>0) {		
+		/*popupate grid with allUsers*/
+		populateGrid(['a','b','c']);
+	} else if ($("#on").is(":checked")) {
+		/*popupate grid with onlineUsers*/
+		populateGrid(['d','e','f']);
+		
+	} else if ($("#off").is(":checked")) {
+		/*popupate grid with offlineUsers*/
+		populateGrid(['g','h','i']);
+		
+	};
+		
 };
 
-
-
-
-  
-  
-/*display function(use id ATTR); if 'display all/online/offline selelcted - populate grid with isonline=true/isonline=false or all*/
+/*call display function as main function */
+/*display all*/
